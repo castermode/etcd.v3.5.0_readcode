8a3b37c163f8cc58c63710cf135a4e47de2f5b26
castermode <castermode@gmail.com>
2025-05-23 12:37:10 +0800

    add code

---
+++ etcd.v3.5.0/my_script/mkdir.sh
@@ -0,0 +1,9 @@
+mkdir -p /tmp/etcd/node1/log
+mkdir -p /tmp/etcd/node2/log    
+mkdir -p /tmp/etcd/node3/log
+
+mkdir -p /tmp/etcd/node1/data
+mkdir -p /tmp/etcd/node2/data
+mkdir -p /tmp/etcd/node3/data
+
+
